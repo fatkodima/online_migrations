3dc04f9e2bf4a70b3373f98aef4b9fc4965e194e
fatkodima <fatkodima123@gmail.com>
2022-01-06 23:08:15 +0200

    bundle gem online_migrations

---
+++ lib/online_migrations.rb
@@ -0,0 +1,6 @@
+require "online_migrations/version"
+
+module OnlineMigrations
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/online_migrations/version.rb
@@ -0,0 +1,3 @@
+module OnlineMigrations
+  VERSION = "0.1.0"
+end
