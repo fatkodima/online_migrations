fatkodima/online_migrations

commit 474e1c1d850aa04be1b0940a1fae379823c7167d

Include operation name into the background schema migrations names
Fixes #71

test/schema_statements/indexes_test.rb
@@ -116,7 +116,7 @@ module SchemaStatements
       @connection.add_index_in_background(:users, :name, unique: true, connection_class_name: "User")
       m = last_schema_migration
 
-      assert_equal "index_users_on_name", m.name
+      assert_equal "Add index index_users_on_name", m.name
       assert_equal "users", m.table_name
       assert_equal 'CREATE UNIQUE INDEX CONCURRENTLY "index_users_on_name" ON "users" ("name")', m.definition
     end
@@ -179,7 +179,7 @@ module SchemaStatements
       @connection.add_index_in_background(:users, :name, name: "my_name", max_attempts: 5, statement_timeout: 10, connection_class_name: "User")
       m = last_schema_migration
 
-      assert_equal "my_name", m.name
+      assert_equal "Add index my_name", m.name
       assert_equal 5, m.max_attempts
       assert_equal 10, m.statement_timeout
     end
@@ -213,7 +213,7 @@ module SchemaStatements
       @connection.remove_index_in_background(:users, :name, name: "index_users_on_name", max_attempts: 5, statement_timeout: 10, connection_class_name: "User")
       m = last_schema_migration
 
-      assert_equal "index_users_on_name", m.name
+      assert_equal "Remove index index_users_on_name", m.name
       assert_equal 5, m.max_attempts
       assert_equal 10, m.statement_timeout
     end
@@ -224,6 +224,15 @@ module SchemaStatements
       assert_raises_with_message(ArgumentError, /when using multiple databases/i) do
         @connection.remove_index_in_background(:users, :name, name: "index_users_on_name")
       end
+    end
+
+    def test_adding_and_removing_index_in_background
+      name = "index_users_on_name"
+      @connection.add_index_in_background(:users, :name, name: name, connection_class_name: "User")
+      assert @connection.index_exists?(:users, :name)
+
+      @connection.remove_index_in_background(:users, :name, name: name, connection_class_name: "User")
+      assert_not @connection.index_exists?(:users, :name)
     end
 
     private

lib/online_migrations/background_schema_migrations/migration_helpers.rb
@@ -28,7 +28,7 @@ module OnlineMigrations
         schema_creation = ActiveRecord::ConnectionAdapters::PostgreSQL::SchemaCreation.new(self)
         definition = schema_creation.accept(create_index)
 
-        enqueue_background_schema_migration(index.name, table_name, definition: definition, **migration_options)
+        enqueue_background_schema_migration("Add index #{index.name}", table_name, definition: definition, **migration_options)
       end
 
       def remove_index_in_background(table_name, column_name = nil, name:, **options)
@@ -42,7 +42,7 @@ module OnlineMigrations
         end
 
         definition = "DROP INDEX CONCURRENTLY IF EXISTS #{quote_column_name(name)}"
-        enqueue_background_schema_migration(name, table_name, definition: definition, **migration_options)
+        enqueue_background_schema_migration("Remove index #{name}", table_name, definition: definition, **migration_options)
       end
 
       def validate_foreign_key_in_background(from_table, to_table = nil, **options)
@@ -62,7 +62,7 @@ module OnlineMigrations
           ALTER TABLE #{quote_table_name(table_name)}
           VALIDATE CONSTRAINT #{quote_table_name(constraint_name)}
         SQL
-        enqueue_background_schema_migration(constraint_name, table_name, definition: definition, **options)
+        enqueue_background_schema_migration("Validate #{constraint_name}", table_name, definition: definition, **options)
       end
 
       # Ensures that the background schema migration with the provided migration name succeeded.
@@ -78,7 +78,7 @@ module OnlineMigrations
       #   ensure_background_schema_migration_succeeded("index_users_on_email")
       #
       def ensure_background_schema_migration_succeeded(migration_name)
-        migrations = Migration.where(migration_name: migration_name).to_a
+        migrations = Migration.where("migration_name ILIKE ?", "%#{migration_name}%").to_a
 
         if migrations.empty?
           Utils.raise_in_prod_or_say_in_dev("Could not find background schema migration(s): '#{migration_name}'.")
@@ -97,11 +97,12 @@ module OnlineMigrations
         if connection_class_name
           klass = connection_class_name.constantize
           connection_class = Utils.find_connection_class(klass)
-          # Normalize to the real connection class name.
-          connection_class_name = connection_class.name
         else
           connection_class = ActiveRecord::Base
         end
+
+        # Normalize to the real connection class name.
+        connection_class_name = connection_class.name
 
         shards = Utils.shard_names(connection_class)
         shards = [nil] if shards.size == 1

test/schema_statements/foreign_keys_test.rb
@@ -69,7 +69,7 @@ module SchemaStatements
       connection.validate_foreign_key_in_background(:milestones, :projects, connection_class_name: "User")
       m = last_schema_migration
 
-      assert_equal "fk_rails_9bd0a0c791", m.name
+      assert_equal "Validate fk_rails_9bd0a0c791", m.name
       assert_equal "milestones", m.table_name
       assert_equal 'ALTER TABLE "milestones" VALIDATE CONSTRAINT "fk_rails_9bd0a0c791"', m.definition
     end
@@ -92,7 +92,7 @@ module SchemaStatements
       )
       m = last_schema_migration
 
-      assert_equal "my_foreign_key", m.name
+      assert_equal "Validate my_foreign_key", m.name
       assert_equal 15, m.max_attempts
     end
   end

test/schema_statements/misc_test.rb
@@ -313,7 +313,7 @@ module SchemaStatements
       @connection.validate_constraint_in_background(:invoices, "fk_invoices_user_id", connection_class_name: Invoice.name)
       m = last_schema_migration
 
-      assert_equal "fk_invoices_user_id", m.name
+      assert_equal "Validate fk_invoices_user_id", m.name
       assert_equal "invoices", m.table_name
       assert_equal 'ALTER TABLE "invoices" VALIDATE CONSTRAINT "fk_invoices_user_id"', m.definition
     end

commit ba3f251d45b57fc483e268c953269902bdc1add8

Fix idempotency for adding/removing indexes in background
Was broken by 474e1c1.

lib/online_migrations/background_schema_migrations/migration.rb
@@ -133,7 +133,7 @@ module OnlineMigrations
 
             with_statement_timeout(connection, statement_timeout) do
               if index_addition?
-                index = connection.indexes(table_name).find { |i| i.name == name }
+                index = connection.indexes(table_name).find { |i| name.include?(i.name) }
                 if index
                   if index.valid?
                     return

test/schema_statements/indexes_test.rb
@@ -131,6 +131,23 @@ module SchemaStatements
       assert_equal 1, OnlineMigrations::BackgroundSchemaMigrations::Migration.count
     end
 
+    def test_adding_index_in_background_is_noop_when_index_already_exists
+      # Emulate created, but not yet executed, schema migration.
+      OnlineMigrations.config.stub(:run_background_migrations_inline, -> { false }) do
+        @connection.add_index_in_background(:users, :name, connection_class_name: "User")
+      end
+
+      assert_not @connection.index_exists?(:users, :name)
+      @connection.add_index(:users, :name)
+      assert @connection.index_exists?(:users, :name)
+
+      migration = OnlineMigrations::BackgroundSchemaMigrations::Migration.last
+      runner = OnlineMigrations::BackgroundSchemaMigrations::MigrationRunner.new(migration)
+      runner.run
+
+      assert @connection.index_exists?(:users, :name)
+    end
+
     def test_add_index_in_background_when_different_index_with_same_name_already_exists
       @connection.add_index(:users, :name, unique: true)
 